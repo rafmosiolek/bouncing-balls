19797fd629cfcb5e5326ecceccf763b36984dbd2
RafM <rkjmosiolek@gmail.com>
2017-04-21 13:34:16 +0200

    an animation loop added

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
 // function to generate random number
-
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
 
+// function constructor for new ball item
 function Ball() {
     this.x = random(0, width);
     this.y = random(0, height);
@@ -22,6 +22,7 @@ function Ball() {
     this.size = random(10, 20);
 }
 
+// function draws ball
 Ball.prototype.draw = function() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -29,6 +30,7 @@ Ball.prototype.draw = function() {
     ctx.fill();
 };
 
+// updating the ball's data, changing velocity when ball reach the edge of the viewport
 Ball.prototype.update = function() {
     if ((this.x + this.size) >= width) {
         this.velX = -(this.velX);
@@ -38,15 +40,54 @@ Ball.prototype.update = function() {
         this.velX = -(this.velX);
     }
 
-    if ((this.y - this.size) <= height) {
+    if ((this.y + this.size) >= height) {
         this.velY = -(this.velY);
     }
 
     if ((this.y - this.size) <= 0) {
-        this.velY = i(this.velY);
-    } 
+        this.velY = -(this.velY);
+    }
 
     this.x += this.velX;
     this.y += this.velY;
 };
+
+// array stores all created balls
+var balls = [];
+
+// an animation loop
+function loop() {
+    // 0.4 value leaves the trail behind the ball
+    ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+    ctx.fillRect(0, 0, width, height);
+
+    // setting up a maximum numbers of balls on the screen
+    while (balls.length < 30) {     
+        var ball = new Ball();
+        balls.push(ball);
+    }
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
