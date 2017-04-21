1ff30204e7c107c64e8ae96b4f2f4fed29351201
RafM <rkjmosiolek@gmail.com>
2017-04-21 12:02:06 +0200

    function constructor Ball() and Ball.draw method added

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+function Ball() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.velX = random(-7, 7);
+    this.velY = random(-7, 7);
+    this.color = "rgb(" + random(0,255) + "," + random(0, 255) + "," + random(0, 255) + ")";
+    this.size = random(10, 20);
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+};
