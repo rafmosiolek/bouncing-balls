a9202910bc58d75488dfe86346b4d996cfc25ff0
RafM <rkjmosiolek@gmail.com>
2017-04-21 14:56:41 +0200

    EvilCircle methods added

--- main.js
+++ main.js
@@ -12,15 +12,28 @@ function random(min,max) {
   return num;
 }
 
-// function constructor for new ball item
-function Ball() {
+// defining Shape constructor
+function Shape() {
     this.x = random(0, width);
     this.y = random(0, height);
     this.velX = random(-7, 7);
     this.velY = random(-7, 7);
+    this.exist = true;
+}
+
+// defining Ball constructor, inheriting from Shape()
+function Ball(x, y, velX, velY, exist) {
+    Shape.call(this, x, y, velX, velY, exist);
+
     this.color = "rgb(" + random(0,255) + "," + random(0, 255) + "," + random(0, 255) + ")";
     this.size = random(10, 20);
 }
+
+// Set Ball()'s constructor and constructor prototype
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
+
+
 
 // function draws ball
 Ball.prototype.draw = function() {
@@ -65,6 +78,83 @@ Ball.prototype.collisionDetect = function() {
             // if collision is detected, balls are changing colours
             if (distance < this.size + balls[j].size) {
                 balls[j].color = this.color = "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")";
+            }
+        }
+    }
+};
+
+
+// defining EvilCircle constructor, inheriting from Shape()
+function EvilCircle(x, y, exist) {
+    Shape.call(this, x, y, exist);
+
+    this.color = "white";
+    this.size = 10;
+    this.velX = 20;
+    this.velY = 20;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+
+// draws the evil circle
+EvilCircle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.lineWidth = 3; // outline width
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+};
+
+// stop the evilcircle from going off the edge of the viewport
+EvilCircle.prototype.checkBounds = function() {
+    if ((this.x + this.size) >= width) {
+        this.velX -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY += this.size;
+    }
+};
+
+// adds onkeydown evenlistener to the window object; evil circle might be moved around now
+EvilCircle.prototype.setControls = function() {
+    var _this = this;
+    window.onkeydown = function(e) {
+        if (e.keyCode === 65) {
+            _this.x -= _this.velX;
+        } else if (e.keyCode === 68) {
+            _this.x += _this.velX;
+        } else if (e.keyCode === 87) {
+            _this.y -= _this.velY;
+        } else if (e.keyCode === 83) {
+            _this.y += _this.velY;
+        }
+    };
+};
+
+// collision detection
+EvilCircle.prototype.collisionDetect = function() {
+    for (var j = 0; j < balls.length; j++) {
+        // checks if the ball being checked exists
+        if (balls[j].exist) {
+            // algorithm checks the collision of two circles (2d collision detection)
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].exist = false;
             }
         }
     }
